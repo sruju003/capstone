c7fc3f85ea2b04b40103375abf707896bc74ce72
vishnuathreya28 <vishnu.athreya77@gmail.com>
2023-11-16 10:51:41 +0530

    added connection to atlas, need to modify the put function which updates count

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, jsonify, render_template
-from pymongo import MongoClient
+from pymongo import MongoClient, server_api
 
 app = Flask(__name__)
 
 # Connect to the MongoDB server
-client = MongoClient('mongodb://127.0.0.1:27017/')
+uri = "mongodb+srv://capstone:capstone1@cluster0.1z4a3y1.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=server_api.ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# client = MongoClient('mongodb://127.0.0.1:27017/')
+
+
 # Create or get a database
 db = client['button_clicks_db']
 
@@ -49,6 +63,28 @@ def get_all_counts():
         all_button_counts[button_id] = click_count
 
     return jsonify(all_button_counts)
+
+# # PATCH route for updating user information based on email
+# @app.route('/update_button_count/<button_id>', methods=['PATCH'])
+# def update_button_count(button_id):
+#     try:
+#         data = request.get_json()
+#         click_count = data.get('click_count')
+
+#         # Find the button
+#         button_temp = button_clicks.find_one({'buttonId': button_id})
+
+#         # Update additional fields
+#         button_temp['button_id'] = button_temp.get('button_count', 0) + click_count
+
+#         # Save the updated user document
+#         button_clicks.update_one({'buttonId': button_id}, {'$set': button_temp})
+
+#         return jsonify({'message': 'Experience, Age, and Data added successfully'}), 200
+
+#     except Exception as error:
+#         print(error)
+#         return jsonify({'error': 'An error occurred while updating user information'}), 500
 
 @app.route('/')
 def index():
