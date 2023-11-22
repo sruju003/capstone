e1c11a5ae8942be6a8e39dd63f4a84589af59b61
vishnuathreya28 <vishnu.athreya77@gmail.com>
2023-11-22 13:12:36 +0530

    this code is incomplete, we need to add face recognition and edit speech1.py to add language changing functionality

--- speech1.py
+++ speech1.py
@@ -2,8 +2,16 @@ import speech_recognition as sr
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from google.cloud import translate_v2 as translate
 
+# Set up the Chrome WebDriver with the path to the ChromeDriver executable
 chrome_path = r'C:\Users\Lenovo\Downloads\chromedriver-win64\chromedriver.exe'
+chrome_options = ChromeOptions()
+chrome_options.add_argument("--headless")  # Add this line if you don't want a visible browser window
+driver = webdriver.Chrome(service=ChromeService(executable_path=chrome_path), options=chrome_options)
+
+# Open the Flask app
+driver.get("http://127.0.0.1:5000/")
 
 def speech_to_text():
     # Initialize the recognizer
@@ -15,7 +23,7 @@ def speech_to_text():
         print("Listening...")
         audio = recognizer.listen(source)
         print("Analyzing...")
-        
+
     try:
         # Recognize speech using Google's Speech Recognition
         text = recognizer.recognize_google(audio)
@@ -26,6 +34,13 @@ def speech_to_text():
     except sr.RequestError as e:
         print("Error with the speech recognition service; {0}".format(e))
         return None
+
+def translate_text(text, target_language="en"):
+    translate_client = translate.Client()
+
+    result = translate_client.translate(text, target_language=target_language)
+
+    return result["input"], result["translatedText"]
 
 def analyze_and_click(text, driver):
     # Analyze the recognized text and perform actions accordingly
@@ -39,18 +54,14 @@ def analyze_and_click(text, driver):
         print("No action specified for the recognized text:", text)
 
 if __name__ == "__main__":
-    # Set up the Chrome WebDriver with the path to the ChromeDriver executable
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument("--headless")  # Add this line if you don't want a visible browser window
-    driver = webdriver.Chrome(service=ChromeService(executable_path=chrome_path), options=chrome_options)
-
-    # Open the Flask app
-    driver.get("http://127.0.0.1:5000/")
-
     while True:
         # Get the recognized text from speech
         recognized_text = speech_to_text()
 
-        # If text is recognized, analyze and perform actions
+        # If text is recognized, translate and analyze
         if recognized_text:
-            analyze_and_click(recognized_text, driver)
+            original_text, translated_text = translate_text(recognized_text, target_language="en")
+            print(f"Original Text: {original_text}")
+            print(f"Translated Text: {translated_text}")
+
+            analyze_and_click(translated_text, driver)
